51ad450a8a24b587bcc036d1f2e486894758ca0e
Beyonce Knowles <beyonce@thinkful.com>
2018-07-01 15:11:10 -0700

    my first test

---
+++ test/test-chase-bank.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect;
+const chase = require('../server')
+
+let jesper = 3;
+
+module.exports = 
+    describe('chase', function() {
+    it('should give right answers for equal and unequal inputs', function(){
+        expect(jesper).to.be.a.name;
+        
+        
+        
+    })
+  }
+);
